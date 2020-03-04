73aab24a098d0ae9859347e5ae5543a102b5d2a8
cedriccarrel <cedi_16@hotmail.com>
2020-03-04 18:16:44 +0100

    Projekvorschlag V2

--- Demos/01_Flask_Demo.py
+++ Demos/01_Flask_Demo.py
@@ -1,8 +1,8 @@
-from flas improt Flask
+from flask import Flask
 app = Flask("Hello World")
 @app.route('/hello')
 def hello_world():
 	return 'Hello, World'
 
-if _name_ == "_main_":
+if __name__ == "__main__":
 	app.run(debug=True, port=5000)
