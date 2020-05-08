7691c89e6bed01463d4c558ce6a55c2801206f96
cedriccarrel <cedi_16@hotmail.com>
2020-05-08 10:03:30 +0200

    Push 08.05.2020

--- Demos/App.py
+++ Demos/App.py
@@ -1,14 +1,48 @@
 from flask import Flask, render_template, redirect, url_for, request
+import preise
+import daten
 
 app = Flask(__name__)
+
+
+@app.route("/speichern/<aktivitaet>")
+def speichern(aktivitaet):
+    zeitpunkt, aktivitaet = daten.aktivitaet_speichern(aktivitaet)
+
+    return "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
+
+
+@app.route("/liste")
+def auflisten():
+    aktivitaeten = daten.aktivitaeten_laden()
+
+    aktivitaeten_liste = ""
+    for key, value in aktivitaeten.items():
+        zeile = str(key) + ": " + value + "<br>"
+        aktivitaeten_liste += zeile
+
+    return aktivitaeten_liste
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == "POST":
-		ziel_person = request.form['vorname']
-		output = "willkommen auf der Webseite" + ziel_person + "!!!"
-		return output
+		erste_zahl = request.form['zahl1']
+		zweite_zahl = request.form['zahl2']
+		ergebnis = float(erste_zahl) + float(zweite_zahl)
+		return str(ergebnis)
 	return render_template("login.html")
+
+@app.route("/<preis>")
+def rabatt(preis):
+	preis_mit_rabatt = preise.rabatt_berechnen(preis)
+	return "Der neue Preis ist" + str(preis_mit_rabatt)
+
+@app.route("/budget")
+def budget():
+	dinger = ["Eins", "Zwei", "Drei"]
+	number = 6
+	return render_template("budget.html", dinger=dinger, number=number)
 
 @app.route('/login2')
 def login2():
---
+++ Demos/preise.py
@@ -0,0 +1,4 @@
+def rabatt_berechnen(preis):
+	preis = int(preis)
+	neuer_preis = preis - (preis * 0.1)
+	return neuer_preis
