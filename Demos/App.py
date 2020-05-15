93bb5135aebc1877759e94e248871d17a3f9998a
cedriccarrel <cedi_16@hotmail.com>
2020-05-15 13:23:53 +0200

    merge2 15.5.

--- Demos/App.py
+++ Demos/App.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, url_for, request
-import preise
 import daten
 import datenbank
 
@@ -14,39 +13,11 @@ def speichern(aktivitaet):
 
 @app.route("/liste2")
 def log():
-	
-
-@app.route("/liste")
-def auflisten():
-    aktivitaeten = daten.aktivitaeten_laden()
-
-    aktivitaeten_liste = ""
-    for key, value in aktivitaeten.items():
-        zeile = str(key) + ": " + value + "<br>"
-        aktivitaeten_liste += zeile
-
-    return aktivitaeten_liste
-
-"""
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if request.method == "POST":
-		erste_zahl = request.form['zahl1']
-		zweite_zahl = request.form['zahl2']
-		ergebnis = float(erste_zahl) + float(zweite_zahl)
-		return str(ergebnis)
-	return render_template("login.html")"""
-
-@app.route("/<preis>")
-def rabatt(preis):
-	preis_mit_rabatt = preise.rabatt_berechnen(preis)
-	return "Der neue Preis ist" + str(preis_mit_rabatt)
+	return render_template("liste2.html")
 
 @app.route("/budget")
 def budget():
-	dinger = ["Eins", "Zwei", "Drei"]
-	number = 6
-	return render_template("budget.html", dinger=dinger, number=number)
+	return render_template("budget.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
--- Demos/blöcke.py
+++
@@ -1,9 +0,0 @@
-from flask import flask
-from flask import render_template
-
-{% include "header.html" %}
-<body>
-    <main>
-        <h1>Hallo</h1>
-    </main>
-{% include "footer.html" %}
