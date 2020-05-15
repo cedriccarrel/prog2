4da112501abcd6849fa9178b2ae0db65719f68fa
cedriccarrel <cedi_16@hotmail.com>
2020-05-15 21:53:54 +0200

    Änderungen 15.05.2020 - Cédric

--- Demos/App.py
+++ Demos/App.py
@@ -5,20 +5,6 @@ import datenbank
 app = Flask(__name__)
 
 
-@app.route("/speichern/<aktivitaet>")
-def speichern(aktivitaet):
-    zeitpunkt, aktivitaet = daten.aktivitaet_speichern(aktivitaet)
-
-    return "Gespeichert: " + aktivitaet + " um " + str(zeitpunkt)
-
-@app.route("/liste2")
-def log():
-	return render_template("liste2.html")
-
-@app.route("/budget")
-def budget():
-	return render_template("budget.html")
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -28,10 +14,6 @@ def login():
         else:
             return redirect(url_for('dashboard'))
     return render_template('login.html', error=error)
-
-@app.route('/index/<name>')
-def index(name):
-	return render_template("index.html", name=name)
 
 @app.route("/")
 @app.route("/dashboard")
@@ -46,6 +28,14 @@ def newtransactions():
 def tasks():
 	return render_template("tasks.html")
 
+@app.route("/budget")
+def budget():
+	return render_template("budget.html")
+
+@app.route('/index/<name>')
+def index(name):
+	return render_template("index.html", name=name)
+	
 	
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
